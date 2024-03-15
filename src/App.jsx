a8d411324f4d34f352608e0a33837209da7e9803
rontysarkar <rontysarkar2023@gmail.com>
2024-03-15 22:26:35 +0600

    add banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+
 import NavBar from './component/NavBar'
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
       <header className='container mx-auto lg:px-32 py-10'>
       <NavBar></NavBar>
       </header>
+      <main className='container mx-auto lg:px32 py-10 '>
+        <div className="bg-img h-40 md:h-80 lg:h-[550px] my-8 bg-no-repeat bg-center bg-contain flex justify-center items-center ">
+            <h1 className='text-4xl text-center  text-white'>Discover an exceptional cooking class tailored for you!</h1>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/component/Banner.jsx
@@ -0,0 +1,11 @@
+
+
+const Banner = () => {
+    return (
+        <div className="" >
+            
+        </div>
+    );
+};
+
+export default Banner;
